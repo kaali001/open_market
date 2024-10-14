72c35f1e94032b417c6825521eb326cf2b922601
Satyendra <21cs2028@rgipt.ac.in>
2024-10-15 01:16:54 +0530

    updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,8 @@
 const config = {
     backendUrl:
       process.env.NODE_ENV === 'production'
-        ? 'https://open-market-server.vercel.app/'
-        : 'https://open-market-server.vercel.app',
+        ? 'https://open-market-server.vercel.app'
+        : 'http://localhost:5000',
   };
   
   export default config;
