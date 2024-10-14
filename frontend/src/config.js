8de512f6781a914676736266965a71568fc96d77
Satyendra <21cs2028@rgipt.ac.in>
2024-10-15 01:10:44 +0530

    server-URL-updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,8 @@
 const config = {
     backendUrl:
       process.env.NODE_ENV === 'production'
-        ? 'https://open-market-server-5dujqme3b-kalis-projects-2db88d8f.vercel.app/'
-        : 'https://open-market-server-5dujqme3b-kalis-projects-2db88d8f.vercel.app',
+        ? 'https://open-market-server.vercel.app/'
+        : 'https://open-market-server.vercel.app',
   };
   
   export default config;
